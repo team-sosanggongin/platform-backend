b5421ad4c79ee4ee848c2f5c85cc19a2012e959c
hy <myName@hello.com>
2026-03-18 16:30:02 +0900

    깃헙엑션을 위한 롤 추가

--- _infra/terraform/oidc.tf
+++ _infra/terraform/oidc.tf
@@ -57,3 +57,25 @@ resource "aws_iam_role_policy" "github_actions_ecr_policy" {
     ]
   })
 }
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.project_name}-${var.env}-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# 2. 표준 정책 연결 (ECR Pull 권한 포함)
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
